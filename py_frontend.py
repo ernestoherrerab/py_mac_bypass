1bd1012dcdd29c2d7e15d3ba71ce3c585793ad20
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2022-03-23 15:37:26 +0100

    Fixed Data Format Manual Input Data

--- py_frontend.py
+++ py_frontend.py
@@ -1,13 +1,20 @@
 import sys
 sys.dont_write_bytecode = True
 from pathlib import Path
-from flask import Flask, redirect, render_template, request
+from decouple import config
+from flask import Flask, redirect, render_template, request, session
 from werkzeug.utils import secure_filename
 import mac_bypass.mac_bypass as bypass
 
 UPLOAD_DIR = Path("csv_data/") 
+GUEST_MAB_ID = config("GUEST_MAB_ID")
+FLASK_KEY = config("SECRET_KEY")
+
+endpoint = {}
+endpoint_list = []
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
+app.secret_key = FLASK_KEY
 
 Path("csv_data/").mkdir(exist_ok=True)
 
@@ -23,32 +30,56 @@ def home_redirect():
 def file_upload():
     return render_template("file_upload.html")
 
-@app.route("/manual_upload")
+@app.route("/manual_upload", methods = ['GET', 'POST'])
 def manual_upload():
     return render_template("manual_upload.html")
 
-@app.route("/uploader", methods = ['GET', 'POST'])
-def uploader():
+@app.route("/ise_login", methods = ['GET', 'POST'])
+def ise_login():
     if request.method == 'GET':
-        return render_template("uploader.html")
+        return render_template("ise_login.html")
     if request.method == 'POST':
-       f = request.files['file']
-       f.save(app.config['UPLOAD_FOLDER'] / secure_filename(f.filename))
-       file_name = f.filename
-       return render_template("uploader.html", file_name=file_name)
+        if "file" in request.files:
+            success = "File Successfully Uploaded!"
+            f = request.files['file']
+            f.save(app.config['UPLOAD_FOLDER'] / secure_filename(f.filename))
+            file_name = f.filename
+            return render_template("ise_login.html", file_name=file_name, success=success)
+        else:
+            text_data = request.form
+            for text in text_data.items():
+                if 'outputtext' in text:
+                    data_input = text[1]
+                    data_input = data_input.replace("\n","").split("\r")
+                    for data in data_input:
+                        data = data.split(",")
+                        if data != ['']:
+                            endpoint["ERSEndPoint"] = {}
+                            mac_add = data[0]
+                            dev_type = data[1]
+                            endpoint["ERSEndPoint"]["name"] = mac_add
+                            endpoint["ERSEndPoint"]["mac"] = mac_add
+                            endpoint["ERSEndPoint"]["profileId"] = dev_type
+                            endpoint["ERSEndPoint"]["staticGroupAssignment"] = "true"
+                            endpoint["ERSEndPoint"]["groupId"] = GUEST_MAB_ID
+                            endpoint_list.append(endpoint)
+            session["endpoint_list"] = endpoint_list
+            return render_template("ise_login.html", endpoint_list=endpoint_list)
 
 @app.route("/ise_auth", methods = ['POST', 'GET'])
 def ise_auth():
     if request.method == 'POST':
-        username=request.form['username']
-        password=request.form['password']
-        result = bypass.mac_bypass(username, password)
-        if result == 401:
-            return render_template("ise_auth_error.html")
-        elif result == {201}:
-            return render_template("ise_upload.html")
-        else: 
-            return render_template("ise_upload_error.html")
+        if "username" in request.form:
+            username=request.form['username']
+            password=request.form['password']
+            result = bypass.mac_bypass(username, password)
+            if result == 401:
+                return render_template("ise_auth_error.html")
+            elif result == {201}:
+                return render_template("ise_upload.html")
+            else: 
+                return render_template("ise_upload_error.html")
+
 
 @app.route("/ise_auth_error")
 def ise_auth_error():
